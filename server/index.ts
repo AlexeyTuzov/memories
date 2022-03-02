c412bfac57a2c02dada66fcd4fc9cb48756c3955
AresNexus <aresnexus@rambler.ru>
2022-03-03 00:22:49 +0500

    Initial commit

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import {connect, set} from 'mongoose';
+import config from 'config';
+
+const app = express();
+
+app.use(bodyParser.json({limit: '30mb'}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+const PORT: number | any = process.env.PORT || 5000;
+connect(config.get('mongoURI'))
+    .then( () => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch( err => {
+        console.log(config.get('mongoURI'));
+        console.log('Connection error:', err.message);
+    });
+
+
+
