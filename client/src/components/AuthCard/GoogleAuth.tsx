caf959fa85cc1c516e1004d721ea6d5da0984907
AlexeyTuzov <aresnexus@rambler.ru>
2022-05-23 12:53:12 +0500

    Initial commit for Google Auth feature

--- client/src/components/AuthCard/GoogleAuth.tsx
+++ client/src/components/AuthCard/GoogleAuth.tsx
@@ -7,13 +7,7 @@ const GoogleAuth: FC = () => {
 
 
     const onButtonClick = () => {
-        gapi.load('client', () => {
-            gapi.auth.signIn({
-                clientid: '1011377274887-45p3iki7eonfi7ap9thsl7pchejcbfo7.apps.googleusercontent.com',
-                cookiepolicy: 'single_host_origin'
-            });
-            console.log('token:', gapi.client.getToken());
-        });
+        
     }
 
     return (
